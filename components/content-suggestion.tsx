bcd6ca02cf1be98ef9dfee0cce329b200f9ce78f
Fabián Karaben <fabiankaraben@gmail.com>
2023-12-07 14:14:52 -0300

    Adds the banner to JavaScript course

---
+++ components/content-suggestion.tsx
@@ -0,0 +1,22 @@
+import type { ReactElement, ReactNode } from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+type ContentSuggestionProps = {
+  num: number
+}
+
+export function ContentSuggestion({
+  num,
+}: ContentSuggestionProps): ReactElement {
+  const ref = useRef(null)
+
+  return (
+    <div className="mt-8 rounded-md border border-yellow-400 border-opacity-75">
+      {/* <Link href="https://esdocu.dev/javascript" target="_blank">
+        <Image width={860} height={0} className="rounded-md border-0" src="/assets/banners/ad-esdocu-dev-javascript-course.svg" alt="Curso de JavaScript" />
+      </Link> */}
+    </div>
+  )
+}
