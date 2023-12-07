e4db7bfa4bcd0c4679ba06732f4fb5daa2636f56
Fabián Karaben <fabiankaraben@gmail.com>
2023-12-07 17:26:39 -0300

    Adds the banner to JavaScript course

--- components/content-suggestion.tsx
+++ components/content-suggestion.tsx
@@ -1,22 +1,79 @@
-import type { ReactElement, ReactNode } from 'react'
-import React, { useRef } from 'react'
+import type { ReactElement } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
 type ContentSuggestionProps = {
   num: number
 }
 
+let nums = []
+
 export function ContentSuggestion({
   num,
 }: ContentSuggestionProps): ReactElement {
-  const ref = useRef(null)
+
+  function randomInt(min: number, max: number): number {
+    return Math.floor((Math.random() * ((max + 1) - min) + min))
+  }
+
+  const articles = [
+    {
+      title: 'Variables en JavaScript',
+      description: 'Una variable es una ubicación con nombre para almacenar datos en la memoria de la computadora.',
+      url: 'https://esdocu.dev/javascript/variables',
+    },
+    {
+      title: 'Operadores básicos en JavaScript',
+      description: 'En esta lección, exploraremos los diferentes tipos de operadores en JavaScript.',
+      url: 'https://esdocu.dev/javascript/operadores-basicos',
+    },
+    {
+      title: 'Funciones en JavaScript',
+      description: 'Las funciones son una forma de agrupar código que se puede reutilizar o ejecutar tantas veces como sea necesario.',
+      url: 'https://esdocu.dev/javascript/funciones',
+    },
+    // {
+    //   title: '',
+    //   description: '',
+    //   url: '',
+    // },
+  ]
+
+  function getArticle() {
+    if (nums.length >= articles.length) nums = []
+    let randNum = -1
+    while (randNum === -1) {
+      let newNum = randomInt(0, articles.length - 1)
+      if (!nums.includes(newNum)) {
+        randNum = newNum
+        nums.push(newNum)
+        console.log(num + ': ' + nums)
+      }
+    }
+
+    return articles[randNum]
+  }
+
+  const [article, setArticle] = useState({ title: '', description: '', url: '' })
+
+  useEffect(() => {
+    setArticle(getArticle)
+  }, [])
 
   return (
-    <div className="mt-8 rounded-md border border-yellow-400 border-opacity-75">
-      {/* <Link href="https://esdocu.dev/javascript" target="_blank">
-        <Image width={860} height={0} className="rounded-md border-0" src="/assets/banners/ad-esdocu-dev-javascript-course.svg" alt="Curso de JavaScript" />
-      </Link> */}
+    <div className="mt-8 px-4 py-5 border-l-4 border-green-600 bg-green-600 bg-opacity-5">
+      <p className="text-xs text-green-600 tracking-widest font-medium title-font mb-2">
+        DE NUESTRO <strong>CURSO DE JAVASCRIPT</strong>
+      </p>
+      <Link href={article.url} target="_blank" className="title-font sm:text-2xl text-xl mb-3 font-medium text-gray-900 dark:text-white">
+        {article.title}
+      </Link>
+      <p className="mb-2">
+        {article.description}
+      </p>
+      <Link href={article.url} target="_blank" className="text-sm text-blue-500 hover:text-blue-700 tracking-widest font-medium title-font">
+        » Aprende más sobre {article.title} ↗
+      </Link>
     </div>
   )
 }
