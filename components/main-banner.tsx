154adbd401247d275cb867a290e70b279cc2751f
Fabián Karaben <fabiankaraben@gmail.com>
2023-12-19 04:24:00 -0300

    Improves some h1 and h2 headings

--- components/main-banner.tsx
+++ components/main-banner.tsx
@@ -9,10 +9,11 @@ export function MainBanner({ }: MainBannerProps): ReactElement {
   const ref = useRef(null)
 
   return (
-    <div className="mt-8 rounded-md border border-yellow-400 border-opacity-75">
-      <Link href="https://esdocu.dev/javascript" target="_blank">
-        <Image width={860} height={0} className="rounded-md border-0" src="/assets/banners/ad-esdocu-dev-javascript-course.svg" alt="Curso de JavaScript" />
-      </Link>
-    </div>
+    <></>
+    // <div className="mt-8 rounded-md border border-yellow-400 border-opacity-75">
+    //   <Link href="https://esdocu.dev/javascript" target="_blank">
+    //     <Image width={860} height={0} className="rounded-md border-0" src="/assets/banners/ad-esdocu-dev-javascript-course.svg" alt="Curso de JavaScript" />
+    //   </Link>
+    // </div>
   )
 }
